df37b5fb4def8b90d2e68667f96d8e9f48834ca7
Lowsun <48923877+thelowsunoverthemoon@users.noreply.github.com>
2024-04-06 19:09:23 -0600

    Delete src/console directory

--- src/console/key.cs
+++
@@ -1,3 +0,0 @@
-if (records[i].EventType==KEY_EVENT) {
-  output.Append("K." + records[i].KeyEvent.wVirtualKeyCode + "." + records[i].KeyEvent.bKeyDown + "." + records[i].KeyEvent.dwControlKeyState + " ");
-}
